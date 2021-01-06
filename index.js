b62f1f4eac4b642dea84bce366a79753ad4ae640
Computer_Q <quinten.muyllaert@outlook.com>
2021-01-06 22:42:12 +0100

    execute different scripts based on turtle label

--- index.js
+++ index.js
@@ -10,23 +10,21 @@ const wss = new WebSocketServer({
 
 const Turtle = require("./turtle.js");
 
-let turtles = [];
-wss.on("request", (request) => {
+wss.on("request", async (request) => {
 	const connection = request.accept();
 
 	let turtle = new Turtle(connection);
-	turtles.push(turtle);
-});
 
-setInterval(async function () {
-	for (let i = 0; i < turtles.length; i++) {
-		let turtle = turtles[i];
-		if (turtle.online) {
-			let b = await turtle.getComputerID();
-			console.log("id", b);
-			b = await turtle.inspect();
-			console.log("block", b);
-			await turtle.print("hello world");
-		}
+	try {
+		const label = await turtle.getComputerLabel();
+
+		let script = require(`./scripts/${label}.js`);
+		delete require.cache[require.resolve(`./scripts/${label}.js`)];
+		script = require(`./scripts/${label}.js`);
+
+		turtle = await script(turtle);
+	} catch (err) {
+		//bleh
+		console.log(err);
 	}
-}, 1000);
+});
